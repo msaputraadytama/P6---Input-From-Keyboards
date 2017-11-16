9d6489d33a1b39c4883251cfbe448735c2d39820
msaputraadytama <32085399+msaputraadytama@users.noreply.github.com>
2017-11-16 14:26:25 +0700

    Add files via upload

---
+++ GetInputReader.java
@@ -0,0 +1,28 @@
+inport java.io.*;
+
+public class GetInputReader{
+    public static void main(String[] args)throws IOException{
+        BufferedReader dataIn = new BufferedReader(new InputStreamReader(System.in));
+        String nim="", nama="", tempat="", tgllahir="";
+
+        System.out.println("Masukan Nama Anda :");
+        nama = dataIn.readLine();
+
+        System.out.println("Masukan NIM Anda :");
+        nim = dataIn.readLine();
+
+        System.out.println("Masukan tempat lahir Anda :");
+        tempat = dataIn.readLine();
+
+        System.out.println("Masukan tanggal lahir Anda :");
+        tgllahir = dataIn.readLine();
+
+        System.out.println("");
+        System.out.println("------------------------");
+        System.out.println("------------------------");
+        System.out.println("Selamat datang " + nama);
+        System.out.println("NIM  : " +nim);
+        System.out.println("Tempat Lahir    : " + tempat);
+        System.out.println("Tanggal Lahir   : " + tgllahhir);
+    }
+}
